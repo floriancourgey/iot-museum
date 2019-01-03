9e27b6c74cff6d51accdbda8fc45406e0095cd04
Florian Courgey <florian@floriancourgey.com>
2019-01-03 23:40:18 +0100

    + v1 with call to server /next

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
+import random
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
-
-artworks = [
-    {'url':'medium.jpg','author':'','title':'Isenheim Altarpiece'},
-    {'url':'medium(1).jpg','author':'','title':'The Tête à Tête'},
-    {'url':'medium(2).jpg','author':'','title':'La Grande Odalisque'},
-    {'url':'medium(3).jpg','author':'','title':'Olympia'},
-    {'url':'medium(4).jpg','author':'','title':'Le Berceau (The Cradle)'},
-]
 
 @app.route('/')
 def index():
     return render_template('index.html',
-        artworks=artworks
+        artworks=getArtworks()
     )
+@app.route('/next')
+def next():
+    artworks = getArtworks()
+    nextIndex = random.randint(0, len(artworks)-1)
+    artworks[nextIndex]['played'] += 1
+    return json.dumps(artworks[nextIndex])
+@app.route('/get/<int:id>')
+def get():
+    pass
+
+def getArtworks():
+    return [
+        {'url':'medium.jpg','author':'','title':'Isenheim Altarpiece','played':0},
+        {'url':'medium(1).jpg','author':'','title':'The Tête à Tête','played':0},
+        {'url':'medium(2).jpg','author':'','title':'La Grande Odalisque','played':0},
+        {'url':'medium(3).jpg','author':'','title':'Olympia','played':0},
+        {'url':'medium(4).jpg','author':'','title':'Le Berceau (The Cradle)','played':0},
+    ]
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
