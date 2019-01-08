cf003cda4231da600a06280639ec0417e1517d97
Florian Courgey <florian@floriancourgey.com>
2019-01-07 23:11:23 -0500

    * crawler artsy with link to db and author fetch

--- crawler-artsy.py
+++ crawler-artsy.py
@@ -3,19 +3,43 @@ import requests
 import json
 import random
 from config import config
+from models import db, Artwork
+from contextlib import suppress
 
 headers = {'X-Xapp-Token': config['artsy']['x_xapp_token']}
-r = requests.get('https://api.artsy.net/api/artworks', headers=headers)
-idx = range(random.randint(1,5))
-print('Rand:', idx)
-for i in idx:
-    next = json.loads(r.text)['_links']['next']['href']
-    print('New url:', next)
-    r = requests.get(next, headers=headers)
-artworks = json.loads(r.text)['_embedded']['artworks']
-html = ''
-for artwork in artworks:
-    html += '<h2>'+artwork['title']+'</h2>'
-    href = artwork['_links']['image']['href']
-    href = href.replace('{image_version}', 'medium')
-    html += '<img src="'+href+'"><br>'
+def getArtsyObject(url):
+    return json.loads(requests.get(url, headers=headers).text)
+
+nextUrl = 'https://api.artsy.net/api/artworks?size=50&offset=15'
+artists = {} # store artists name
+for i in range(0,1):
+    print('Next url:', nextUrl)
+    response = getArtsyObject(nextUrl)
+    artworks = response['_embedded']['artworks']
+    for a in artworks:
+        o = Artwork()#origin='artsy')
+        with suppress (KeyError, IndexError):
+            o.name = a['title']
+        # get author via API call (or cache)
+        with suppress (KeyError, IndexError):
+            href = a['_links']['artists']['href']
+            if not href in artists:
+                artist = getArtsyObject(href)
+                artists[href] = artist['_embedded']['artists'][0]['name']
+            o.author = artists[href]
+        with suppress (KeyError, IndexError):
+            href = a['_links']['image']['href']
+            if 'large' in a['image_versions']:
+                href = href.replace('{image_version}', 'large')
+            else:
+                href = href.replace('{image_version}', a['image_versions'][0])
+            o.url = href
+        if not o.name or not o.url:
+            continue
+        print(o)
+        existing = db.session.query(Artwork).filter(Artwork.url==o.url).first()
+        if not existing:
+            db.session.add(o)
+    db.session.commit()
+    # next
+    nextUrl = response['_links']['next']['href']
