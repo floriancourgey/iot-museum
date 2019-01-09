54414eef74a51d1ef2916699d21a939390acbcb3
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 14:47:31 -0500

    + / and /next fetch only active artworks

--- museum/views.py
+++ museum/views.py
@@ -9,17 +9,17 @@ from django.conf import settings
 class Index(View):
     def get(self, request):
         return render(request, 'index.html', {
-            'numberOfArtworks': Artwork.objects.count()
+            'numberOfArtworks': Artwork.objects.filter(active=1).count()
         })
 
 class Next(View):
-    def get(self,request):
+    def get(self, request):
         # get count of smallest "timesPlayed"
-        smallestTimesPlayed = Artwork.objects.order_by('timesPlayed').first().timesPlayed
-        countOfSmallest = Artwork.objects.filter(timesPlayed=smallestTimesPlayed).count()
+        smallestTimesPlayed = Artwork.objects.filter(active=1).order_by('timesPlayed').first().timesPlayed
+        countOfSmallest = Artwork.objects.filter(timesPlayed=smallestTimesPlayed, active=1).count()
         # get random artwork
         offset = random.randint(0, countOfSmallest-1)
-        artwork = Artwork.objects.filter(timesPlayed=smallestTimesPlayed)[offset:offset+1].get()
+        artwork = Artwork.objects.filter(timesPlayed=smallestTimesPlayed, active=1)[offset:offset+1].get()
         # increment timesPlayed
         artwork.timesPlayed += 1
         artwork.save()
