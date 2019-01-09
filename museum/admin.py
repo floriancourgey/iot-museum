dfd9e0a31d6c8d9e26989ff09d89623f38bb09df
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 14:56:09 -0500

    + admin displays timesPlayed

--- museum/admin.py
+++ museum/admin.py
@@ -3,8 +3,8 @@ from .models import Artwork
 
 @admin.register(Artwork)
 class ArtworkAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'active', 'created_datetime', 'edited_datetime')
+    list_display = ('name', 'author', 'active', 'timesPlayed', 'created_datetime', 'edited_datetime')
     list_editable = ('active',)
-    list_filter = ('active',)
+    list_filter = ('active','timesPlayed')
     list_per_page = 20
     search_fields = ('name', 'author', 'url')
