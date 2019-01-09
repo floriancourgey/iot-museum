08528601810df44682c26ad3118b207e7080295a
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 15:25:30 -0500

    + custom admin_login template

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.login_template = 'admin_login.html'
+
 urlpatterns = [
     path('', include('museum.urls')),
     path('admin/', admin.site.urls),
