875335d1d535d209fc7bcc184d38b2f22eab226b
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 13:45:54 -0500

    + views as View class

--- museum/urls.py
+++ museum/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('next', views.next, name='next'),
+    path('', views.Index.as_view(), name='index'),
+    path('next', views.Next.as_view(), name='next'),
 ]
--- museum/views.py
+++ museum/views.py
@@ -1,24 +1,27 @@
 from django.core import serializers
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 from .models import Artwork
 import random
 from django.conf import settings
 
-def index(request):
-    return render(request, 'index.html', {
-        'numberOfArtworks': Artwork.objects.count()
-    })
-def next(request):
-    # print('API_KEY',settings.API_KEY)
-    # get count of smallest "timesPlayed"
-    smallestTimesPlayed = Artwork.objects.order_by('timesPlayed').all()[0].timesPlayed
-    countOfSmallest = Artwork.objects.filter(timesPlayed=smallestTimesPlayed).count()
-    # get random artwork
-    offset = random.randint(0, countOfSmallest-1)
-    artwork = Artwork.objects.filter(timesPlayed=smallestTimesPlayed)[offset:offset+1].get()
-    # increment timesPlayed
-    artwork.timesPlayed += 1
-    artwork.save()
-    # return as json
-    return JsonResponse(artwork.as_dict())
+class Index(View):
+    def get(self, request):
+        return render(request, 'index.html', {
+            'numberOfArtworks': Artwork.objects.count()
+        })
+
+class Next(View):
+    def get(self,request):
+        # get count of smallest "timesPlayed"
+        smallestTimesPlayed = Artwork.objects.order_by('timesPlayed').first().timesPlayed
+        countOfSmallest = Artwork.objects.filter(timesPlayed=smallestTimesPlayed).count()
+        # get random artwork
+        offset = random.randint(0, countOfSmallest-1)
+        artwork = Artwork.objects.filter(timesPlayed=smallestTimesPlayed)[offset:offset+1].get()
+        # increment timesPlayed
+        artwork.timesPlayed += 1
+        artwork.save()
+        # return as json
+        return JsonResponse(artwork.as_dict())
