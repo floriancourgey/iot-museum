89b279f2d62a5ed7d89cdbb982bfd1153cb8da9e
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 16:27:09 -0500

    + docs/index.js

---
+++ docs/index.js
@@ -0,0 +1,67 @@
+var artworks = [];
+$.getJSON('artworks.json', function(data){
+  artworks = data;
+  play();
+});
+// if of the tiemout process (to be stopped when Pause)
+var timeoutId = null;
+// array of displayed artwork
+var artworkHistory = [];
+// jquery objects
+var $img = null;
+var $name = null;
+var $author = null;
+var $timeoutInterval = null;
+var $pause = null;
+var $play = null;
+// functions
+function previous(){
+  pause();
+  if(artworkHistory.length < 1){
+    return;
+  }
+  artwork = artworkHistory.pop();
+  setArtwork(artwork);
+}
+function pause(){
+  $pause.hide();
+  $play.show();
+  clearTimeout(timeoutId);
+}
+function play(){
+  $play.hide();
+  $pause.show();
+  next();
+}
+function next(){
+  console.log('next() called');
+  var nextIndex = parseInt(Math.random()*artworks.length);
+  var artwork = artworks[nextIndex];
+  setArtwork(artwork);
+  artworkHistory.push(artwork);
+  clearTimeout(timeoutId);
+  timeoutInterval = parseInt($timeoutInterval.val());
+  if(timeoutInterval <= 0){
+    timeoutInterval = 10;
+  }
+  console.log('next() using timeoutInterval='+timeoutInterval);
+  timeoutId = setTimeout(function(){
+    console.log('setTimeout() called');
+    next();
+  }, timeoutInterval*1000);
+  console.log('next() timeoutId='+timeoutId);
+}
+function setArtwork(a) {
+  $img.attr('src', a.url);
+  $name.html(a.name);
+  $author.html(a.author);
+}
+$(function(){
+  $img = $('#img');
+  $name = $('#name');
+  $author = $('#author');
+  $timeoutInterval = $('#timeoutInterval');
+  $pause = $('#pause');
+  $play = $('#play');
+  //play();
+});
