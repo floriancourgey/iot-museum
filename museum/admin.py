5836d767c59bcc99dac217bdc1ab81ad1f1b77d2
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-09 19:09:31 -0500

    + admin/museum/artwork/rmngp to add artworks from RMNGP

--- museum/admin.py
+++ museum/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Artwork
+from django.urls import path
+from django.template.response import TemplateResponse
 
 @admin.register(Artwork)
 class ArtworkAdmin(admin.ModelAdmin):
@@ -8,3 +10,17 @@ class ArtworkAdmin(admin.ModelAdmin):
     list_filter = ('active','timesPlayed')
     list_per_page = 20
     search_fields = ('name', 'author', 'url')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('rmngp/', self.admin_site.admin_view(self.rmngp)),
+        ]
+        return my_urls + urls
+
+    def rmngp(self, request):
+        context = dict(
+           self.admin_site.each_context(request),
+           # key=value,
+        )
+        return TemplateResponse(request, "admin_rmngp.html", context)
