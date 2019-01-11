db46dd48f4725ef455397a38ac160e42a8c5c34b
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-11 16:08:48 -0500

    + django rest framework

--- settings.py
+++ settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -64,3 +65,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from past.builtins import execfile
 
 urlpatterns = [
     path('', include('museum.urls')),
     path('admin/', admin.site.urls),
 ]
+
+execfile('urls_api.py')
