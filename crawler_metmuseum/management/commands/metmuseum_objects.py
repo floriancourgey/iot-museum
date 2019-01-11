19900faf136d6ec0c04535f3031b3807a3aadf0f
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-11 16:38:24 -0500

    + adding local cache for crawler_metmuseum

--- crawler_metmuseum/management/commands/metmuseum_objects.py
+++ crawler_metmuseum/management/commands/metmuseum_objects.py
@@ -1,6 +1,19 @@
 import json
 import requests
+import yaml
+import os
 from django.core.management.base import BaseCommand, CommandError
+
+cacheFilename = os.path.dirname(os.path.realpath(__file__))+'/cache.yml'
+cacheFile = open(cacheFilename, 'a+') # a+ to create if not existing
+cacheFile.seek(0, 0)
+cache = yaml.load(cacheFile) or {'minId':0, 'highlightedArtworksIds':[]}
+cacheFile.close()
+
+def saveCache():
+    cacheFile = open(cacheFilename, 'w')
+    yaml.dump(cache, cacheFile)
+    cacheFile.close()
 
 def getJson(url):
     print('Calling '+url)
@@ -8,23 +21,35 @@ def getJson(url):
     return json.loads(r.text)
 
 class Command(BaseCommand):
-    help = 'Usage: python manage.py metmuseum_objects'
+    help = 'Usage: python manage.py metmuseum_objects. See https://metmuseum.github.io/ for details.'
 
     def handle(self, *args, **options):
-        minId = 40000
+        # get all
         result = getJson('https://collectionapi.metmuseum.org/public/collection/v1/objects')
         print('Found '+str(result['total'])+' objects')
         objectIDs = result['objectIDs']
         print('URL response OK')
         i = 0
         for id in objectIDs:
-            if id < minId:
+            if id < cache['minId']:
                 continue
+            cache['minId'] = id
+            saveCache()
+            # get details
             object = getJson('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+str(id))
-            # print(object)
             if not object['isHighlight']:
                 continue
+            cache['highlightedArtworksIds'].append({
+                'objectID': object['objectID'],
+                'title': object['title'],
+                'artistDisplayName': object['artistDisplayName'],
+                'objectDate': object['objectDate'],
+            })
+            saveCache()
             i += 1
+            print('===========')
             print(i, object['title'])
+            print(object)
+            print('===========')
             if i >= 10:
                 exit()
