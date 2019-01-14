daa33a4e64380f8df1124a0f6d593697843817cd
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-14 15:43:11 -0500

    + tests with django manage.py test

--- museum/test_museum.py
+++ museum/test_museum.py
@@ -1,2 +1,24 @@
-def test_hello():
-    return True
+import json
+from django.test import TestCase, Client
+from .models import Artwork
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+        Artwork.objects.create(name='La Joconde', author='Leonard de Vinci')
+        Artwork.objects.create(name='La nuit etoilee', author='Vincent Van Gogh')
+        Artwork.objects.create(name='Londres, le Parlement', author='Claude Monet')
+
+    def test_timesPlayed0(self):
+        self.assertEqual(Artwork.objects.order_by('timesPlayed').first().timesPlayed, 0)
+        self.assertEqual(Artwork.objects.order_by('timesPlayed').last().timesPlayed, 0)
+
+
+    def test_next_timesPlayed(self):
+        response = self.c.get('/next')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(json.loads(response.content), dict)
+        response = self.c.get('/next')
+        response = self.c.get('/next')
+        self.assertEqual(Artwork.objects.order_by('timesPlayed').first().timesPlayed, 1)
+        self.assertEqual(len(Artwork.objects.filter(timesPlayed=1)), 3)
