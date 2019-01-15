edd317082ba2ef6a8a83740d4bc8a20ff140614b
Florian Courgey <florian@floriancourgey.com>
2019-01-14 22:27:30 -0500

    + Artwork.origin_id

--- museum/models.py
+++ museum/models.py
@@ -7,12 +7,15 @@ class Artwork(models.Model):
     url = models.URLField(max_length=255)
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    timesPlayed = models.IntegerField(default=0)
-    origin = models.CharField(max_length=255)
+    timesPlayed = models.IntegerField(default=0) # number of times this artwork has been played
+    origin = models.CharField(max_length=255) # origin system (crawler site, backoffice..)
+    origin_id = models.CharField(max_length=255, default=None, null=True) # id in the origin system
     active = models.BooleanField(default=True)
+
     def as_dict(self):
         d = self.__dict__
         del d['_state']
         return d
+
     def __str__(self):
         return '"'+self.name+'" by '+str(self.author)
