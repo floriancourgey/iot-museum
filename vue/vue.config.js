efd585aff5c64332a8b43f551166076ff1995a5e
Florian Courgey <florian@floriancourgey.com>
2019-01-18 22:52:01 -0500

    first test

---
+++ vue/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  outputDir: '../dist',
+  publicPath: '/static/',
+  filenameHashing: false,
+  pages: {
+    index: {
+      entry: 'src/museum-front/main.js',
+      template: 'templates/museum-front.html',
+      filename: '../museum/templates/museum/index.html', // relative to dist
+    },
+  },
+}
