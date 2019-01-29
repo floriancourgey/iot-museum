0cdd72d8e6a4d8171f0b7471a386f9f54928ca0e
Florian Courgey <florian@floriancourgey.com>
2019-01-28 22:01:32 -0500

    + admin displays artworks date

--- museum/admin.py
+++ museum/admin.py
@@ -6,7 +6,7 @@ from config import config
 
 @admin.register(Artwork)
 class ArtworkAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'active', 'timesPlayed', 'created_datetime', 'edited_datetime')
+    list_display = ('name', 'author', 'active', 'date_display', 'timesPlayed', 'created_datetime', 'edited_datetime')
     list_editable = ('active',)
     list_filter = ('active','timesPlayed')
     list_per_page = 20
