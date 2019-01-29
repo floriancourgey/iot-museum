c56ffbf6f5905de465970dff3404b35c843d0c61
Florian Courgey <florian@floriancourgey.com>
2019-01-28 21:45:52 -0500

    ! functions of crawler RmnGP with new config obj

--- crawler_rmngp/management/commands/functions.py
+++ crawler_rmngp/management/commands/functions.py
@@ -1,9 +1,12 @@
 import requests
 import json
-from config import *
+from config import config
 
-headers = {'ApiKey': RMNGP_API_KEY}
+headers = {'ApiKey': config['rmngp']['api_key']}
 def getRmngpObject(resource):
     url = 'https://api.art.rmngp.fr/v1/'+resource.replace(' ', '%20')
     print('Calling '+url)
-    return json.loads(requests.get(url, headers=headers).text)
+    obj = json.loads(requests.get(url, headers=headers).text)
+    if 'error' in obj:
+        raise Exception('RmnGP API error:', obj['error'])
+    return obj
