a48177073406140566007ace7bea11120e887355
Florian Courgey <florian@floriancourgey.com>
2020-12-28 20:18:20 +0100

    + title & author

--- museum/static/index.jQuery.js
+++ museum/static/index.jQuery.js
@@ -4,6 +4,8 @@ var timeoutId = null;
 var timeoutInterval = 15; // in sec
 var img = jQuery('#img');
 img.css('height', window.innerHeight); // fix for old TV browsers
+var artwork_name = jQuery('#artwork_name');
+var artwork_author = jQuery('#artwork_author');
 
 function next(){
   debug('next() called');
@@ -20,6 +22,8 @@ function next(){
       debug('json');
       debug(JSON.stringify(data));
       img.attr('src', data.url_online);
+      artwork_name.text(data.name+' ');
+      artwork_author.text(data.author+' ');
     });
 
   // if(this.timeoutInterval <= 0){
