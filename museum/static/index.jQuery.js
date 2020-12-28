aad4450c7429ad3a3ee69334e9d26c8077e3eabb
Florian Courgey <florian@floriancourgey.com>
2020-12-28 19:38:47 +0100

    + refonte jQuery

---
+++ museum/static/index.jQuery.js
@@ -0,0 +1,29 @@
+var artworkHistory = [];
+
+function next(){
+  debug('next() called');
+  // if(this.isPaused){
+  //   console.log('next() exit because app.isPaused');
+  //   return;
+  // }
+  // artworkHistory.push(app.artwork);
+  // clearTimeout(this.timeoutId);
+  jQuery.get(urlNext)
+    .success(function (data) {
+      debug('data');
+      debug(data);
+      debug('json');
+      debug(JSON.stringify(data));
+    });
+
+  // if(this.timeoutInterval <= 0){
+  //   this.timeoutInterval = 10;
+  // }
+  // this.timeoutId = setTimeout(function(){
+  //   console.log('setTimeout() called');
+  //   app.next();
+  // }, this.timeoutInterval*1000);
+  // console.log('next() timeoutId='+this.timeoutId);
+}
+
+next();
